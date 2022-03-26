d8b5ad13f04f46a21a7ce3a96ae04bd20aeb9787
heycn <x1chennan@qq.com>
2022-03-27 00:14:38 +0800

    完善'添加待办事项'输入的逻辑

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import TopBar from './component/TopBar/TopBar'
 import AddTodo from './component/AddTodo/AddTodo'
 
 const App = () => {
   const [addTodoVisible, setAddTodoVisible] = useState(false)
+  const [todoList, setTodoList] = useState([])
 
   const addTodoSwitch = () => setAddTodoVisible(!addTodoVisible)
+  const addListItem = useCallback(value => {
+    const dataItem = {
+      id: new Date().getTime(),
+      content: value,
+      completed: false
+    }
+
+    setTodoList(todoList => [...todoList, dataItem])
+    setAddTodoVisible(false)
+  }, [])
+
   return (
     <div className='App'>
       <TopBar addTodoSwitch={addTodoSwitch} />
-      <AddTodo addTodoVisible={addTodoVisible} />
+      <AddTodo addTodoVisible={addTodoVisible} addListItem={addListItem} />
     </div>
   )
 }
--- src/component/AddTodo/AddTodo.js
+++ src/component/AddTodo/AddTodo.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './style.scss'
 
 const AddTodo = props => {
-  const { addTodoVisible } = props
+  const { addTodoVisible, addListItem } = props
+  const inputRef = useRef()
+
+  const onSubmitValue = () => {
+    const inputValue = inputRef.current.value.trim()
+    if (inputValue.length === 0) {
+      return
+    }
+
+    addListItem(inputValue)
+
+    inputRef.current.value = ''
+  }
+
   return (
     <>
       {addTodoVisible && (
         <div className='add-todo-wrapper'>
-          <input type='text' placeholder='点击添加待办事件...' />
-          <button className='button button-primary'>添加</button>
+          <input type='text' ref={inputRef} placeholder='点击添加待办事项...' />
+          <button className='button button-primary' onClick={onSubmitValue}>
+            添加
+          </button>
         </div>
       )}
     </>
